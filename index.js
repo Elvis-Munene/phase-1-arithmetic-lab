fc4b5e6615b759e4a85395c4d01d62bd656e1dcd
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-09 10:39:43 +0300

    commit tree a5edaee200c31f657a3c3d81ac6c027925ed4eac

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1*num2;
+console.log(multiply);
+
+const random = Math.floor((Math.random()*10));
+console.log(random);
+
+let num3=10
+let num4=6
+const mod = num3%num4;
+
+console.log(mod);
+
+
+var max = Math.max(10,16,0,1,3,20);
+console.log(max);
